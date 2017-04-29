7c3767e3cfb3b4e6c796ca5e8906f979a7efb2b2
kurenai0413 <kurenai0413@hotmail.com>
2017-04-29 11:31:16 +0800

    Tapping two time to calculate the 3d coordinate fo the four corner points.

--- Assets/Scripts/kuGestureAction.cs
+++ Assets/Scripts/kuGestureAction.cs
@@ -65,7 +65,7 @@ namespace kuHoloLensUtility
             ImgWidth  = c.cameraResolutionWidth;
             ImgHeight = c.cameraResolutionHeight;
 
-            DebugText.text = ImgWidth + " x " + ImgHeight;
+            //sDebugText.text = ImgWidth + " x " + ImgHeight;
 
             captureObject.StartPhotoModeAsync(c, OnPhotoModeStarted);
         }
@@ -104,7 +104,9 @@ namespace kuHoloLensUtility
 
             if (result.success)
             {
-                DebugText.text = "Capturing....";
+                
+                photoCaptureFrame.TryGetCameraToWorldMatrix(out CamToWorldMat);
+                photoCaptureFrame.TryGetProjectionMatrix(out ProjMat);
 
                 #region // Copy capture to texture. //
                 // Create our Texture2D for use and set the correct resolution
@@ -121,8 +123,7 @@ namespace kuHoloLensUtility
                 photoCaptureFrame.CopyRawImageDataIntoBuffer(imageBufferList);
                 #endregion
 
-                DebugText.text = "Copy to byte.\n";
-
+                //DebugText.text = "Copy to byte.\n";
 
 #if !UNITY_EDITOR
 #if Homography
@@ -136,7 +137,6 @@ namespace kuHoloLensUtility
                     Debug.Log(res.Text);//將解碼後的資料列印出來
 
                     BarcodeText.text = "Barcode Text: " + res.Text + "\n";
-                    DebugText.text = string.Empty;
                     //BarcodeText.text += "PtsNum: " + res.ResultPoints.Length + "\n";
                     BarcodeText.text += "Detected result points:\n";
 
@@ -170,11 +170,33 @@ namespace kuHoloLensUtility
 
                         BorderInFramePts.Add((double)item);
                     }
+
+                
+                    Vector2 qPt1 = new Vector2((float)BorderInFramePts.ElementAt(0),
+                                               (float)BorderInFramePts.ElementAt(1));
+
+                    DebugText.text += "qPt1: " + qPt1.x + ", " + qPt1.y + "\n";                  
+                    kuDrawRaycast(qPt1, 0.01f);
+
+                    //Vector2 qPt2 = new Vector2((float)BorderInFramePts.ElementAt(2),
+                    //                           (float)BorderInFramePts.ElementAt(3));    
+                    //DebugText.text += "qP21: " + qPt2.x + ", " + qPt2.y + "\n";
+                    //kuDrawRaycast(qPt2);
+
+                    //Vector2 qPt3 = new Vector2((float)BorderInFramePts.ElementAt(4),
+                    //                           (float)BorderInFramePts.ElementAt(5));    
+                    //DebugText.text += "qPt3: " + qPt3.x + ", " + qPt3.y + "\n";
+                    //kuDrawRaycast(qPt3);
+
+                    //Vector2 qPt4 = new Vector2((float)BorderInFramePts.ElementAt(6),
+                    //                           (float)BorderInFramePts.ElementAt(7));    
+                    //DebugText.text += "qPt4: " + qPt4.x + ", " + qPt4.y + "\n";
+                    //kuDrawRaycast(qPt4);
                 }
                 else
                 {
                     BarcodeText.text = "Not detected.";
-                    DebugText.text   = "Not detected.";
+                    DebugText.text   += "Not detected.";
                 }
 #endif
 #endif
@@ -212,6 +234,139 @@ namespace kuHoloLensUtility
             return from;
         }
 
+
+        private void kuDrawRaycast(Vector2 qPt, float CubeSize)
+        {
+            Vector2 ImagePosZeroToOne = new Vector2(qPt.x / ImgWidth,
+                                                    (float)(1.0 - (qPt.y / (float)ImgHeight)));
+            Vector2 ImagePosProjected = new Vector2((float)(ImagePosZeroToOne.x * 2.0 - 1.0),
+                                                    (float)(ImagePosZeroToOne.y * 2.0 - 1.0));
+            Vector3 aaa = new Vector3(ImagePosProjected.x, ImagePosProjected.y, 1.0f);
+            Vector3 CamSpacePos = UnProjectVector(ProjMat, aaa);
+            Vector3 CameraSpaceOrigin = new Vector3(0, 0, 0);
+            Vector3 WorldSpaceRayPoint1 = CamToWorldMat.MultiplyPoint3x4(CameraSpaceOrigin);
+            // camera location in world space
+            Vector3 WorldSpaceRayPoint2 = CamToWorldMat.MultiplyPoint3x4(CamSpacePos);
+            // ray point in world space
+
+            Vector3 OriVec = WorldSpaceRayPoint2 - WorldSpaceRayPoint1;
+
+            GameObject CamPosCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            CamPosCube.transform.position = WorldSpaceRayPoint1;
+            CamPosCube.transform.localScale = new Vector3(CubeSize, CubeSize, CubeSize);   // 單位是公尺
+            CamPosCube.transform.forward = OriVec;
+            CamPosCube.GetComponent<Renderer>().material.color = Color.blue;
+
+            DebugText.text += "WorldSpaceRayPoint1: ("
+                            + WorldSpaceRayPoint1.x + ", "
+                            + WorldSpaceRayPoint1.y + ", "
+                            + WorldSpaceRayPoint1.z + ")\n";
+            
+
+            GameObject WorldPtCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            WorldPtCube.transform.position = WorldSpaceRayPoint2;
+            WorldPtCube.transform.localScale = new Vector3(CubeSize, CubeSize, CubeSize);   // 單位是公尺
+            WorldPtCube.transform.forward = OriVec;
+            WorldPtCube.GetComponent<Renderer>().material.color = Color.red;
+
+            DebugText.text += "WorldSpaceRayPoint2: ("
+                            + WorldSpaceRayPoint2.x + ", "
+                            + WorldSpaceRayPoint2.y + ", "
+                            + WorldSpaceRayPoint2.z + ")\n";
+
+            
+
+            //DebugText.text += "OriVec: ("
+            //                + OriVec.x + ", "
+            //                + OriVec.y + ", "
+            //                + OriVec.z + ")\n";
+
+            //Ray CornerRay = new Ray(WorldSpaceRayPoint2, OriVec);
+            //RaycastHit HitInfo;
+            //bool HitBool = Physics.Raycast(CornerRay, out HitInfo, 15.0f, Physics.DefaultRaycastLayers);
+
+            //RaycastHit [] Hits = Physics.RaycastAll(rr);
+
+            if (OriVec.magnitude != 1.0f)
+            {
+                float length = OriVec.magnitude;
+                OriVec /= length;
+            }
+
+            GameObject LineCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            LineCube.transform.position = WorldSpaceRayPoint1;
+            LineCube.transform.forward = OriVec;
+            LineCube.transform.localScale = new Vector3(0.1f * CubeSize, 0.1f * CubeSize, 100.0f);   // 單位是公尺
+
+            //DebugText.text += "Hit: " + HitBool.ToString() + "\n";
+
+            //if (HitBool)
+            //{
+            //    DebugText.text += "HitInfo: " +
+            //    HitInfo.transform.position.x + ", " +
+            //    HitInfo.transform.position.y + ", " +
+            //    HitInfo.transform.position.z + "\n";
+
+            //    GameObject cubeFront = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    cubeFront.transform.position = HitInfo.transform.position;
+            //    cubeFront.transform.forward = OriVec;
+            //    cubeFront.transform.localScale = new Vector3(2 * CubeSize, 2 * CubeSize, 2 * CubeSize);   // 單位是公尺
+            //    cubeFront.GetComponent<Renderer>().material.color = Color.cyan;
+
+            //}
+
+            //kuDrawLine(OriVec, WorldSpaceRayPoint1, 1000, 0.01f);
+        }
+
+        private void kuDrawLine(Vector3 OriVec, Vector3 CenterPt, int Length, float thickness)
+        {
+            //if (OriVec.magnitude != 1.0f)
+            //{
+            //    float length = OriVec.magnitude;
+            //    OriVec /= length;
+            //    //OriVec *= thickness;
+            //}
+
+            DebugText.text += "RR\n";
+
+            Ray rr = new Ray(CenterPt, OriVec);
+            RaycastHit HitInfo;
+            bool HitBool = Physics.Raycast(rr, out HitInfo, 10);
+            DebugText.text += "Hit: " + HitBool.ToString() + "\n";
+            DebugText.text += "HitInfo: " +
+                HitInfo.transform.position.x + ", " +
+                HitInfo.transform.position.y + ", " +
+                HitInfo.transform.position.z + "\n";
+
+            GameObject cubeFront = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cubeFront.transform.position = HitInfo.transform.position;
+            cubeFront.transform.forward = OriVec;
+            cubeFront.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);   // 單位是公尺
+
+
+            //Vector3 FrontLinePt = new Vector3();
+            //Vector3 BackLinePt = new Vector3();
+
+            //for (int i=0;i<Length; i++)
+            //{
+            //    FrontLinePt = CenterPt + (i * OriVec);
+            //    BackLinePt = CenterPt - (i * OriVec);
+
+            //    //GameObject cubeFront = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    //cubeFront.transform.position = FrontLinePt;
+            //    //cubeFront.transform.forward = OriVec;
+            //    //cubeFront.transform.localScale = new Vector3(thickness, thickness, thickness);   // 單位是公尺
+
+            //    GameObject cubeBack = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //    cubeBack.transform.position = BackLinePt;
+            //    cubeBack.transform.forward = OriVec;
+            //    cubeBack.transform.localScale = new Vector3(thickness, thickness, thickness);   // 單位是公尺
+
+            //}
+
+
+        }
+
         // Update is called once per frame
         void Update()
         {
@@ -221,6 +376,12 @@ namespace kuHoloLensUtility
         void OnTappedEvent()
         {
             DebugText.text = "Tapped.";
+
+            DebugText.text = string.Empty;
+            DebugText.text += "Camera Point: "
+                            + Camera.main.transform.position.x + ", "
+                            + Camera.main.transform.position.y + ", "
+                            + Camera.main.transform.position.z + "\n";
 
             PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
         }
